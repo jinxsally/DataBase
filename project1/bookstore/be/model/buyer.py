fa371bd92b3130b46c74eb7a803bfde7f3e9c455
fWang-0406 <10223330406@stu.ecnu.edu.cn>
2024-10-21 17:50:10 +0800

    buyer.py增加取消订单的函数cancel_order和自动取消订单的函数auto_cancel_order

--- project1/bookstore/be/model/buyer.py
+++ project1/bookstore/be/model/buyer.py
@@ -2,7 +2,7 @@
 import uuid
 import json
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 from pymongo import errors
 
 from project1.bookstore.be.model import db_conn
@@ -179,7 +179,7 @@ class Buyer(db_conn.DBConn):
                     "details": details
                 })
 
-                # 查询已付款订单，根据不同的状态
+            # 查询已付款订单，根据不同的状态
             status_mapping = {1: "unsent", 2: "sent but not received", 3: "received"}
             query_paid = {"user_id": user_id, "status": {"$in": [1, 2, 3]}}
             result_paid = self.conn.order_col.find(query_paid)
@@ -198,7 +198,7 @@ class Buyer(db_conn.DBConn):
                     "details": details
                 })
 
-                # 查询已取消订单
+            # 查询已取消订单
             query_cancelled = {"user_id": user_id, "status": 4}
             result_cancelled = self.conn.order_col.find(query_cancelled)
             for order in result_cancelled:
@@ -228,3 +228,112 @@ class Buyer(db_conn.DBConn):
             return 200, "ok", "No orders found"
         else:
             return 200, "ok", ans
+
+    def cancel_order(self, user_id: str, order_id: str) -> (int, str):
+        try:
+            # 未付款
+            result = self.conn.order_col.find_one({"order_id": order_id, "status": 0})
+            if result:
+                buyer_id = result.get("user_id")
+                if buyer_id != user_id:
+                    return error.error_authorization_fail()
+                store_id = result.get("store_id")
+                price = result.get("price")
+                self.conn.order_col.delete_one({"order_id": order_id, "status": 0})
+            # 已付款
+            else:
+                result = self.conn.order_col.find_one({
+                    "$or": [
+                        {"order_id": order_id, "status": 1},
+                        {"order_id": order_id, "status": 2},
+                        {"order_id": order_id, "status": 3},
+                    ]
+                })
+                if result:
+                    buyer_id = result.get("user_id")
+                    if buyer_id != user_id:
+                        return error.error_authorization_fail()
+                    store_id = result.get("store_id")
+                    price = result.get("price")
+
+                    result1 = self.conn.store_col.find_one({"store_id": store_id})
+
+                    if result1 is None:
+                        return error.error_non_exist_store_id(store_id)
+                    seller_id = result1.get("user_id")
+
+                    result2 = self.conn.user_col.update_one({"user_id": seller_id}, {"$inc": {"balance": -price}})
+                    if result2 is None:
+                        return error.error_non_exist_user_id(seller_id)
+
+
+                    result3 = self.conn.user_col.update_one({"user_id": buyer_id}, {"$inc": {"balance": price}})
+                    if result3 is None:
+                        return error.error_non_exist_user_id(user_id)
+
+                    result4 = self.conn.order_col.delete_one({
+                    "$or": [
+                        {"order_id": order_id, "status": 1},
+                        {"order_id": order_id, "status": 2},
+                        {"order_id": order_id, "status": 3},
+                    ]
+                })
+                    if result4 is None:
+                        return error.error_invalid_order_id(order_id)
+
+                else:
+                    return error.error_invalid_order_id(order_id)
+
+            # 调整库存
+            result = self.conn.order_detail_col.find({"order_id": order_id})
+            for book in result:
+                book_id = book["book_id"]
+                count = book["count"]
+                result1 = self.conn.store_col.update_one({"store_id": store_id, "books.book_id": book_id}, {"$inc": {"books.$.stock_level": count}})
+                if result1.modified_count == 0:
+                    return error.error_stock_level_low(book_id) + (order_id,)
+
+            self.conn.order_col.insert_one({"order_id": order_id, "user_id": user_id, "store_id": store_id, "price": price, "status": 4})
+
+        except errors.PyMongoError as e:
+            # debug
+            print(e)
+            return 528, "{}".format(str(e))
+        except BaseException as e:
+            # debug
+            print(e)
+            return 530, "{}".format(str(e))
+        return 200, "ok"
+
+    def auto_cancel_order(self) -> (int, str):
+        try:
+            wait_time = 20  # 等待时间20s
+            interval = datetime.utcnow() - timedelta(seconds=wait_time) # UTC时间
+            orders_to_cancel = self.conn.order_col.find({"create_time": {"$lte": interval}, "status": 0})
+            if orders_to_cancel:
+                for order in orders_to_cancel:
+                    order_id = order["order_id"]
+                    user_id = order["user_id"]
+                    store_id = order["store_id"]
+                    price = order["price"]
+                    self.conn.order_col.delete_one({"order_id": order_id, "status": 0})
+                    result = self.conn.order_detail_col.find({"order_id": order_id})
+
+                    for book in result:
+                        book_id = book["book_id"]
+                        count = book["count"]
+                        result1 = self.conn.store_col.update_one({"store_id": store_id, "books.book_id": book_id}, {"$inc": {"books.$.stock_level": count}})
+                        if result1.modified_count == 0:
+                            return error.error_stock_level_low(book_id) + (order_id,)
+
+                    self.conn.order_col.insert_one({"order_id": order_id, "user_id": user_id,"store_id": store_id, "price": price, "status": 4})
+
+        except errors.PyMongoError as e:
+            # debug
+            print(e)
+            return 528, "{}".format(str(e))
+        except BaseException as e:
+            # debug
+            print(e)
+            return 530, "{}".format(str(e))
+        return 200, "ok"
