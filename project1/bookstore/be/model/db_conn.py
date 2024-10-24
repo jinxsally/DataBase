ae0f912c14cae162c5beffa3a0db1e7b8ea23a52
jinxsally <hqg13550019896@163.com>
2024-10-24 17:07:21 +0800

    test 79%

--- project1/bookstore/be/model/db_conn.py
+++ project1/bookstore/be/model/db_conn.py
@@ -3,35 +3,35 @@ from be.model import store
 
 class DBConn:
     def __init__(self):
-        self.conn = store.get_db_conn()
+        self.client = store.get_db_conn()
+        self.db = self.client
 
     def user_id_exist(self, user_id):
-        cursor = self.conn.execute(
-            "SELECT user_id FROM user WHERE user_id = ?;", (user_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
+        doc = self.db["users"].find_one({"user_id": user_id})
+        if doc is None:
             return False
         else:
             return True
 
     def book_id_exist(self, store_id, book_id):
-        cursor = self.conn.execute(
-            "SELECT book_id FROM store WHERE store_id = ? AND book_id = ?;",
-            (store_id, book_id),
+        doc = self.db["stores"].find_one(
+            {"store_id": store_id, "books.book_id": book_id}
         )
-        row = cursor.fetchone()
-        if row is None:
+        if doc is None:
             return False
         else:
             return True
 
     def store_id_exist(self, store_id):
-        cursor = self.conn.execute(
-            "SELECT store_id FROM user_store WHERE store_id = ?;", (store_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
+        doc = self.db["user_store"].find_one({"store_id": store_id})
+        if doc is None:
+            return False
+        else:
+            return True
+
+    def order_id_exist(self, order_id):
+        doc = self.db["new_orders"].find_one({"order_id": order_id})
+        if doc is None:
             return False
         else:
             return True
--- project1/bookstore/be/model/user.py
+++ project1/bookstore/be/model/user.py
@@ -1,9 +1,10 @@
 import jwt
 import time
 import logging
-import sqlite3 as sqlite
 from be.model import error
 from be.model import db_conn
+import pymongo
+import pymongo.errors
 
 # encode a json string like:
 #   {
@@ -19,7 +20,7 @@ def jwt_encode(user_id: str, terminal: str) -> str:
         key=user_id,
         algorithm="HS256",
     )
-    return encoded.decode("utf-8")
+    return encoded
 
 
 # decode a JWT to a json string like:
@@ -39,6 +40,7 @@ class User(db_conn.DBConn):
     def __init__(self):
         db_conn.DBConn.__init__(self)
 
+    # 检查给定的JWT是否有效
     def __check_token(self, user_id, db_token, token) -> bool:
         try:
             if db_token != token:
@@ -53,43 +55,46 @@ class User(db_conn.DBConn):
             logging.error(str(e))
             return False
 
+    # 注册
     def register(self, user_id: str, password: str):
         try:
             terminal = "terminal_{}".format(str(time.time()))
             token = jwt_encode(user_id, terminal)
-            self.conn.execute(
-                "INSERT into user(user_id, password, balance, token, terminal) "
-                "VALUES (?, ?, ?, ?, ?);",
-                (user_id, password, 0, token, terminal),
-            )
-            self.conn.commit()
-        except sqlite.Error:
+            doc = {
+                "_id": user_id,
+                "user_id": user_id,
+                "password": password,
+                "balance": 0,
+                "token": token,
+                "terminal": terminal,
+            }
+            self.db["users"].insert_one(doc)
+        except pymongo.errors.PyMongoError as e:
             return error.error_exist_user_id(user_id)
         return 200, "ok"
 
+    # 检查权限
     def check_token(self, user_id: str, token: str) -> (int, str):
-        cursor = self.conn.execute("SELECT token from user where user_id=?", (user_id,))
-        row = cursor.fetchone()
-        if row is None:
+        doc = self.db["users"].find_one({"user_id": user_id}, {"token": 1, "_id": 0})
+        if doc is None:
             return error.error_authorization_fail()
-        db_token = row[0]
+        db_token = doc["token"]
         if not self.__check_token(user_id, db_token, token):
             return error.error_authorization_fail()
         return 200, "ok"
 
+    # 检查密码
     def check_password(self, user_id: str, password: str) -> (int, str):
-        cursor = self.conn.execute(
-            "SELECT password from user where user_id=?", (user_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
+        doc = self.db["users"].find_one({"user_id": user_id}, {"password": 1, "_id": 0})
+        if doc is None:
             return error.error_authorization_fail()
 
-        if password != row[0]:
+        if password != doc["password"]:
             return error.error_authorization_fail()
 
         return 200, "ok"
 
+    # 登录
     def login(self, user_id: str, password: str, terminal: str) -> (int, str, str):
         token = ""
         try:
@@ -98,19 +103,20 @@ class User(db_conn.DBConn):
                 return code, message, ""
 
             token = jwt_encode(user_id, terminal)
-            cursor = self.conn.execute(
-                "UPDATE user set token= ? , terminal = ? where user_id = ?",
-                (token, terminal, user_id),
+            result = self.db["users"].update_one(
+                {"user_id": user_id}, {"$set": {"token": token, "terminal": terminal}}
             )
-            if cursor.rowcount == 0:
+            if result.modified_count == 0:
                 return error.error_authorization_fail() + ("",)
-            self.conn.commit()
-        except sqlite.Error as e:
+
+        except pymongo.errors.PyMongoError as e:
             return 528, "{}".format(str(e)), ""
         except BaseException as e:
+            print(e)
             return 530, "{}".format(str(e)), ""
         return 200, "ok", token
 
+    # 登出
     def logout(self, user_id: str, token: str) -> bool:
         try:
             code, message = self.check_token(user_id, token)
@@ -120,37 +126,36 @@ class User(db_conn.DBConn):
             terminal = "terminal_{}".format(str(time.time()))
             dummy_token = jwt_encode(user_id, terminal)
 
-            cursor = self.conn.execute(
-                "UPDATE user SET token = ?, terminal = ? WHERE user_id=?",
-                (dummy_token, terminal, user_id),
+            result = self.db["users"].update_one(
+                {"user_id": user_id},
+                {"$set": {"token": dummy_token, "terminal": terminal}},
             )
-            if cursor.rowcount == 0:
+            if result.modified_count == 0:
                 return error.error_authorization_fail()
-
-            self.conn.commit()
-        except sqlite.Error as e:
+        except pymongo.errors.PyMongoError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
         return 200, "ok"
 
+    # 注销
     def unregister(self, user_id: str, password: str) -> (int, str):
         try:
             code, message = self.check_password(user_id, password)
             if code != 200:
                 return code, message
 
-            cursor = self.conn.execute("DELETE from user where user_id=?", (user_id,))
-            if cursor.rowcount == 1:
-                self.conn.commit()
-            else:
+            result = self.db["users"].delete_one({"user_id": user_id})
+            if result.deleted_count == 0:
                 return error.error_authorization_fail()
-        except sqlite.Error as e:
+
+        except pymongo.errors.PyMongoError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
         return 200, "ok"
 
+    # 修改密码
     def change_password(
         self, user_id: str, old_password: str, new_password: str
     ) -> bool:
@@ -161,16 +166,83 @@ class User(db_conn.DBConn):
 
             terminal = "terminal_{}".format(str(time.time()))
             token = jwt_encode(user_id, terminal)
-            cursor = self.conn.execute(
-                "UPDATE user set password = ?, token= ? , terminal = ? where user_id = ?",
-                (new_password, token, terminal, user_id),
+            result = self.db["users"].update_one(
+                {"user_id": user_id},
+                {
+                    "$set": {
+                        "password": new_password,
+                        "token": token,
+                        "terminal": terminal,
+                    }
+                },
             )
-            if cursor.rowcount == 0:
+            if result.modified_count == 0:
                 return error.error_authorization_fail()
-
-            self.conn.commit()
-        except sqlite.Error as e:
+        except pymongo.errors.PyMongoError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
         return 200, "ok"
+
+    # 搜索
+    def search(self, user_id: str, store_id: str, key: str, page: int, page_size: int):
+        try:
+            if not self.user_id_exist(user_id):
+                # debug
+                return error.error_non_exist_user_id(user_id)
+            if not self.store_id_exist(store_id):
+                # debug
+                return error.error_non_exist_store_id(store_id)
+
+            # 执行查询操作，两种情况
+
+            # 店铺搜索
+            if store_id:
+                query = {
+                    "store_id": store_id,
+                    "books.stock_level": {"$gt": 0},  # 库存量大于等于0
+                    "books": {
+                        "$elemMatch": {
+                            "book_info": {
+                                "$regex": key,  # 存在关键词的都需要
+                                "$options": "i",  # 不区分大小写
+                            }
+                        }
+                    },
+                }
+                books = (
+                    self.db.stores.find(query)
+                    .skip((page - 1) * page_size)  # 跳过已经浏览过的界面
+                    .limit(page_size)  # 限制浏览数量
+                )
+
+            # 全局搜索
+            else:
+                query = {
+                    "books.stock_level": {"$gt": 0},  # 库存量大于等于0
+                    "books": {
+                        "$elemMatch": {
+                            "book_info": {
+                                "$regex": key,
+                                "$options": "i",
+                            }
+                        }
+                    },
+                }
+                books = (
+                    self.db.stores.find(query)
+                    .skip((page - 1) * page_size)  # 跳过已经浏览过的界面
+                    .limit(page_size)  # 限制浏览数量
+                )
+
+            # 筛选执行结果的信息
+            ans = []
+
+            for book in books:
+                ans.append(book)
+
+        except pymongo.errors.PyMongoError as e:
+            return 528, "{}".format(str(e))
+        except BaseException as e:
+            return 530, "{}".format(str(e))
+        return 200, "ok", ans
